cf17a7ba02800e87a2d1ef1072ad73e371696629
carlhernek <53989735+carlhernek@users.noreply.github.com>
2020-08-31 13:34:28 +0200

    Bugfix #1
    CUSTOM_ELEMENTS_SCHEMA added to allow for import of my cms library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,5 +11,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [BrowserModule, AppRoutingModule, ThirdCmsModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
